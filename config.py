8080a343358ae280a028dbcc27f85abcad0dd517
Rishabh <152057079+Rishabhopi@users.noreply.github.com>
2025-01-20 13:37:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,17 +6,17 @@ if os.path.exists("local.env"):
     load_dotenv("local.env")
 
 
-API_ID = int(getenv("API_ID", "27079591")) #optional
-API_HASH = getenv("API_HASH", "c81ae4c3dc026ea4bf49842a8ce4a5f9") #optional
+API_ID = int(getenv("API_ID", "14050586")) #optional
+API_HASH = getenv("API_HASH", "42a60d9c657b106370c79bb0a8ac560c") #optional
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5738579437").split()))
 OWNER_ID = int(getenv("OWNER_ID", "5738579437"))
-MONGO_URL = getenv("MONGO_URL", "mongodb+srv://TEAMBABY01:UTTAMRATHORE09@cluster0.vmjl9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+MONGO_URL = getenv("MONGO_URL", "mongodb+srv://Krishna:pss968048@cluster0.4rfuzro.mongodb.net/?retryWrites=true&w=majority")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
 ALIVE_PIC = getenv("ALIVE_PIC", 'https://envs.sh/oKU.jpg')
 ALIVE_TEXT = getenv("ALIVE_TEXT")
 PM_LOGGER = getenv("PM_LOGGER")
-LOG_GROUP = getenv("LOG_GROUP")
+LOG_GROUP = getenv("LOG_GROUP","-1001992970818")
 GIT_TOKEN = getenv("GIT_TOKEN") #personal access token
 REPO_URL = getenv("REPO_URL", "https://github.com/Rishuz/Slove")
 BRANCH = getenv("BRANCH", "main") #don't change
