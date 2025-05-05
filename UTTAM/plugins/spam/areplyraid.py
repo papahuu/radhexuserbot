76258de1b41e3b76d75c2656023789ed86e49938
TheRadhe <rajveer202415@gmail.com>
2025-05-06 00:27:14 +0530

    Update areplyraid.py

--- UTTAM/plugins/spam/areplyraid.py
+++ UTTAM/plugins/spam/areplyraid.py
@@ -1,55 +1,65 @@
-import os
-import sys
 import asyncio
-import re
 from random import choice
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from cache.data import *
-from UTTAM.database.rraid import *
+from UTTAM.database.rraid import get_rraid_users, rraid_user
 from UTTAM import SUDO_USER
-from pyrogram import Client, errors, filters
-from pyrogram.types import ChatPermissions, Message
-DEVS = int(8093150680)
-from UTTAM.helper.PyroHelpers import get_ub_chats
-from UTTAM.plugins.basic.profile import extract_user, extract_user_and_reason
-SUDO_USERS = SUDO_USER
 from .replyraid import RAIDS
+from UTTAM.plugins.basic.profile import extract_user
 
+# Make sure SUDO_USER is a list of ints
+SUDO_USERS = SUDO_USER if isinstance(SUDO_USER, list) else [SUDO_USER]
 
-
-@Client.on_message(
-    filters.command(["replyraid"], ".") & (filters.me | filters.user(SUDO_USER))
-)
-async def gmute_user(client: Client, message: Message):
+@Client.on_message(filters.command(["replyraid"], ".") & (filters.me | filters.user(SUDO_USERS)))
+async def activate_replyraid(client: Client, message: Message):
     args = await extract_user(message)
     reply = message.reply_to_message
     ex = await message.edit_text("`Processing...`")
-    if args:
-        try:
-            user = await client.get_users(args)
-        except Exception:
-            await ex.edit(f"`Please specify a valid user!`")
-            return
-    elif reply:
-        user_id = reply.from_user.id
-        user = await client.get_users(user_id)
-    else:
-        await ex.edit(f"`Please specify a valid user!`")
-        return
-    if user.id == client.me.id:
-        return await ex.edit("**Okay Sure.. 🐽**")
-    elif user.id == SUDO_USERS:
-        return await ex.edit("**Okay But Failed Because this user in sudos.. 🐽**")
-    elif user.id == VERIFIED_USERS:
-        return await ex.edit("**Chal Chal Baap ko Mat sikha.. 🐽**")
+
     try:
+        if args:
+            user = await client.get_users(args)
+        elif reply:
+            user = reply.from_user
+        else:
+            return await ex.edit("`Please specify a valid user!`")
+
+        if user.id == client.me.id:
+            return await ex.edit("**Okay Sure.. 🐽**")
+        if user.id in SUDO_USERS:
+            return await ex.edit("**Failed! This user is a sudo user.. 🐽**")
+
         if user.id in (await get_rraid_users()):
-           await ex.edit("Replyraid is activated on this user")
-           return
+            return await ex.edit("Replyraid is already activated on this user")
+
         await rraid_user(user.id)
         RAIDS.append(user.id)
-        await ex.edit(f"[{user.first_name}](tg://user?id={user.id}) Activated ReplyRaid!")
+
+        await ex.edit(f"[{user.first_name}](tg://user?id={user.id}) is now under ReplyRaid!")
     except Exception as e:
         await ex.edit(f"**ERROR:** `{e}`")
+
+# This listener actually performs the raid
+@Client.on_message(filters.group & filters.text)
+async def replyraid_action(client: Client, message: Message):
+    user_id = message.from_user.id if message.from_user else None
+    if not user_id:
         return
+
+    # Check if user is in active replyraid list
+    if user_id in RAIDS or user_id in (await get_rraid_users()):
+        try:
+            await asyncio.sleep(1)  # Optional delay
+            reply = await message.reply(
+                choice([
+                    "Chup baith ja!",
+                    "Teri bakwaas sunne ka time nahi.",
+                    "Phirse bola to gaali padegi!",
+                    "Aukat me reh.",
+                    "Ek number ka chomu hai tu!"
+                ])
+            )
+            await asyncio.sleep(10)
+            await reply.delete()
+        except Exception as e:
+            print(f"[ReplyRaid Error] {e}")
